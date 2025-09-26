59855e2026ea106241345be02652501a0925f17a
Zaibi448 <ukzoraib@gmail.com>
2025-09-26 10:14:15 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "ZORAIB-XD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiaUp5S2VCZjd6M2ZMYUpQUDJySW5XQnI2NjMydk5CZi9oOU5iWTlCSzQzST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSVpFNXpQc3Z2eERrYUJnUGIxYmNGNjVySGZrcTE5K2lhcGpnYnRJQUh3ND0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJJRkZaZGZNVXh1YTRxYWxGNnp6QmdmOEpNMndlQmZNdkRZTnZ1NitLb1VvPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ5Qm9QSS9VYnZXQkgxWlo2UFRWYytRRFEwSGdUNmFYR09ham5yWEhlMG1VPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InlDaDNGWlZpYTFuemd6Y3k5VHFqcGVRYnlvQ1c1dTh4bWF6ZlRyaUs2WDg9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImZQUFREQTVwMGxyMGJ2UFNPYlRITFg4M3R6Tk83SVBLTnNBWEVIcFBhQ0k9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiaUpJYldqZUJsYmhGOVhNajdma3RhZmo3OWpUYUpuY1pTb0l4cVpKM2RVND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNzBWb09OUlY0NTJ1SWJobWNmQXc4V1RLUVJBcTdVK1VhQy9xeHh6VjNrTT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFJTEJzeXBUTVFra1BYV21vYVUxK1FpK1MvTm1scVU2a2JlVllZOGh2aDdHakhPQkRVMGp1eVZJVHh2TndUMDJnRktJaDlsY0dsMkVsdE55MHN2RGp3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NDAsImFkdlNlY3JldEtleSI6ImloN2I2eWRFSm55ZVJXVXJaMnphV1BZcmwvcTBCdlBYaTdkRTV5OHdITUU9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbeyJrZXkiOnsicmVtb3RlSmlkIjoiOTIzMjQ3OTQ3MjM4QHMud2hhdHNhcHAubmV0IiwiZnJvbU1lIjp0cnVlLCJpZCI6IkFDQkU1MEZGRkI0NDg3OTRCMjkwNjA5MEREM0E0N0ZFIn0sIm1lc3NhZ2VUaW1lc3RhbXAiOjE3NTg3ODQwNTl9XSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IkFWWE1WWDdNIiwibWUiOnsiaWQiOiI5MjMyNDc5NDcyMzg6MTBAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIyMTkxMjU2NDExNjI5Njc6MTBAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNKaWE5KzhCRUxYVTA4WUdHQU1nQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJOVVg1elFvUEh2UkRuOWp5bS93dlg0ZFdQOFJYTHUxYjQwWnAzZ0NBNERNPSIsImFjY291bnRTaWduYXR1cmUiOiJERUJENXVHY2xyYTFYTUtXbFdPSlVKbUpNLzFJdkNyd2JWWVhBdVAxS2Z6eHJoY2RqdTJUME5ZbGtab1NkSTQ2eDFsWm9DUEFRL2xzYkpueGcrbjVEQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoic2NiVmlGOFFDTGlSMVJycllUZlhQUThCaytjb2JpYjBYaEVqcEQrVVF4WFNzQTlMYlZkVmJHeXlOMndrYkJ1NUhwNzZXcUdDWDJYSVJsaWxwdjNwZ0E9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMyNDc5NDcyMzg6MTBAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVFZGK2MwS0R4NzBRNS9ZOHB2OEwxK0hWai9FVnk3dFcrTkdhZDRBZ09BeiJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0EwSUNBPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzU4Nzg0MDU1LCJsYXN0UHJvcEhhc2giOiJQV2s1QiIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBTjJsIn0=",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+// make true if you want auto reply on status 
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto reply on status 
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY HASSU-XD 🇵🇰*",
+// set the auto reply massage on status reply  
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+// set true false for anti delete     
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
+// change it to 'same' if you want to resend deleted message in same chat     
+WELCOME: process.env.WELCOME || "false",
+// true if want welcome and goodbye msg in groups    
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+// make true to know who dismiss or promoted a member in group
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+// make true if want auto voice reply if someone menetion you 
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/yrpxq6.jpg",
+// add custom menu and mention reply image url
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+BOT_NAME: process.env.BOT_NAME || "HASSU-XD",
+// add bot namw here for menu
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// true to get auto status react
+STICKER_NAME: process.env.STICKER_NAME || "HASSU-XD",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself 
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
+// automatic delete links witho remove member 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045919",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "HASSU-XD Official",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 HASSU-XD*",
+// add bot owner name    
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/yrpxq6.jpg",
+// add img for alive msg
+LIVE_MSG: process.env.LIVE_MSG || "> I'm alive *HASSU-XD*🇵🇰",
+// add alive msg here 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words  
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "true",
+// make anti link true,false for groups 
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+DEV: process.env.DEV || "923357304126",
+//replace with your whatsapp number        
+ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+// make it true for auto recoding 
+};
