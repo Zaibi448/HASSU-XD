1cf29700ee55e6661fd496eb7e62ce7f5c08cba7
Zaibi448 <ukzoraib@gmail.com>
2025-09-26 11:38:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const {
   const path = require('path')
   const prefix = config.PREFIX
   
-  const ownerNumber = ['923142854207']
+  const ownerNumber = ['923073660479']
   
   const tempDir = path.join(os.tmpdir(), 'cache-temp')
   if (!fs.existsSync(tempDir)) {
@@ -69,7 +69,7 @@ const {
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
     if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
     
-    const sessdata = config.SESSION_ID.replace("ZORAIB-XD~", '');
+    const sessdata = config.SESSION_ID.replace("ARSLAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMk4yOW5hM0dsZ1pKSDdEVHVENTd5YytKSThhMitUYWRTVlQ5cHdSRG9Ybz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidWM5dmlERzhnM3FzVHNoYnhJWFFPWFlEVzd0bTUxQzdXVU1ZWWJ3b0d3MD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDSVFabU9MUkgrMnprdzNQS01hY2FRQ0dZbG1jWTJnUkJQbGJ3TE5Va1ZZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhVHMyaDZveUt4NjdYUkQwZGhrL0VjNXl1OENNSENQcEVQM3cxYU1ZVGpJPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InVIVm5YRmE2Mm5OZXdFcEhwQVFvdlpkbkxoTHVwUldRVStBV00yQk12bmM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlBCVHZlYlhOdXdFWUxBZG0reExDaGJOT3ZFTVhaUEZNKzEvQktORUJFQ2c9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS0hrc2Q2NkxDN3pJZ3NkYThiaERiZ1NGc3ZqcTQwSzZKV1BrK0dlZENIST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZWc3RWJzZXVtdHZZYlozaGUveVBRMFFUQlloUnFIRzZIZGVLblRNM3pFdz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkZXckZwZTJoOGdkdzFCU0xscjl0ZHVIVE53Sk5SWWhkblhab0VKSVlhaC9zZGp4eU9pd2FGb3BiSzVxSVJGVnZTWGo3SEY1eUZYMSs5L3VVcXFzT0RRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6OTMsImFkdlNlY3JldEtleSI6IkdxTzkvblQ5ZzRWcVZWOHpVc2hlNThIaHloTSsxNVZTR3FiOW1WbTFFNXc9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IloxMzU0Mk1TIiwibWUiOnsiaWQiOiI5MjMyNDc5NDcyMzg6MTJAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIyMTkxMjU2NDExNjI5Njc6MTJAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNKcWE5KzhCRVAzYTJNWUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJOVVg1elFvUEh2UkRuOWp5bS93dlg0ZFdQOFJYTHUxYjQwWnAzZ0NBNERNPSIsImFjY291bnRTaWduYXR1cmUiOiJUU0l6T1RwVWpGOWkzZTltallaYndXQ0l1U1gyUXR4RURpeXlvRWNwM2FUVzdCdHAxMFBlWEJNV1pJTjROK2ZJRG5TNkNERkV5WGVKUmVqSU9ZbVBEQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoibzB0clNQRUxUQzFQdEtkWkdnSVFHN0VlZzNPV21rb3lWNVpYeEpuajdyaVFIWFlHbnFkelpIem14Z1pRakE3RVhCcGtzZWpEMUFscXU2TlZsWGd6Q3c9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMyNDc5NDcyMzg6MTJAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVFZGK2MwS0R4NzBRNS9ZOHB2OEwxK0hWai9FVnk3dFcrTkdhZDRBZ09BeiJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0EwSUNBPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzU4ODY2ODE4LCJsYXN0UHJvcEhhc2giOiJQV2s1QiJ9~", '');
     try {
         // Decode base64 string
         const decodedData = Buffer.from(sessdata, 'base64').toString('utf-8');
@@ -120,8 +120,8 @@ const port = process.env.PORT || 9090;
   console.log('Plugins installed successful ✅')
   console.log('Bot connected to whatsapp ✅')
   
-  let up = `*THANKS FOR CHOOSING HASSU-XD! \ud83d\udc4b\ud83c\udffb* \n\n> Simple , Straight Forward But Loaded With Features \ud83c\udf8a, Meet HASSU-XD WhatsApp Bot.\n\n *ZORAIB-KASHMIRI\ud83d\udea9* \n\n> Join WhatsApp Channel :- ⤵️\n \https://whatsapp.com/channel/0029VbAzg127oQhgVzEERx2E\n\n- *YOUR PREFIX:* = ${prefix}\n\nDont forget to give star to repo ⬇️\n\nhttps://github.com/Zaibi448/HASSU-XD.git\n\n> © `POWERED BYE ZORAIB KASHMIRI` Official ❣️ \ud83d\udda4`;
-    conn.sendMessage(conn.user.id, { image: { url: `https://files.catbox.moe/qeq0cg.jpg` }, caption: up })
+  let up = `*Hello there HASSU-XD User! \ud83d\udc4b\ud83c\udffb* \n\n> Simple , Straight Forward But Loaded With Features \ud83c\udf8a, Meet HASSU-XD WhatsApp Bot.\n\n *Thanks for using HASSU-XD \ud83d\udea9* \n\n> Join WhatsApp Channel :- ⤵️\n \nhttps://whatsapp.com/channel/0029VbAt6PxK0IBkzjPPMf1x \n\n- *YOUR PREFIX:* = ${prefix}\n\nDont forget to give star to repo ⬇️\n\nhttps://github.com/Zaibi448/HASSU-XD\n\n> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ HASSU-XD Official 🫰 \ud83d\udda4`;
+    conn.sendMessage(conn.user.id, { image: { url: `https://files.catbox.moe/ynqcjl.jpg` }, caption: up })
   }
   })
   conn.ev.on('creds.update', saveCreds)
@@ -727,7 +727,7 @@ if (!isReact && config.CUSTOM_REACT === 'true') {
             for (let i of kon) {
                 list.push({
                     displayName: await conn.getName(i + '@s.whatsapp.net'),
-                    vcard: `BEGIN:VCARD\nVERSION:1.0\nN:${await conn.getName(
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await conn.getName(
                         i + '@s.whatsapp.net',
                     )}\nFN:${
                         global.OwnerName
@@ -776,7 +776,7 @@ if (!isReact && config.CUSTOM_REACT === 'true') {
   }
   
   app.get("/", (req, res) => {
-  res.send("HASSU-XD STARTED ✅");
+  res.send("E3-WAQAR-MD STARTED ✅");
   });
   app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
   setTimeout(() => {
